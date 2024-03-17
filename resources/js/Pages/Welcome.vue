1cf150ed6495f4b6652a8a29210e8b221e328809
Alan <alanrodrigo.mg@gmail.com>
2024-03-17 13:11:02 -0400

    Instalando laravel jetstream con inertia y tailwindCSS video 11 mejorando tambien la pagina welcome

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+});
+</script>
+
+<template>
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+            Welcome Alan
+        </div>
+    </div>
+</template>
